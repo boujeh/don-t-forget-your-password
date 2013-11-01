c94626cfa45741094ab7071e1eca253e327d6819
boujeh <mohamed.boujeh@esprit.tn>
2013-11-01 15:24:55 -0700

    Create page_del.php

---
+++ page_del.php
@@ -0,0 +1,9 @@
+<DOCTYPE HTML>
+<html>
+<body>
+<form method="post" action="search.php">
+name of website : <input type="text" name="name_site" />
+<input type="submit" value="Ok" />
+</form>
+</body>
+</html>
